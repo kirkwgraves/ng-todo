618b55684d961d46ffc95bd14b551d81bdba8c3d
Kirk Graves <graves.kirk@gmail.com>
2015-11-09 19:45:24 -0600

    added two-way data binding in pastactrl

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var app = angular.module('TodoApp', []);
 
-app.controller('TodoCtrl', function($scope){
+app.controller('TodoCtrl', function($scope, $rootScope){
 	$scope.title = 'Welcome to your Todo App';
+	$rootScope.hello = 'I am everywhere';
+})
+
+app.controller('PastaCtrl', function($scope){
+	$scope.pasta = 'I love carbs';
+	$scope.foods = '';
 })
